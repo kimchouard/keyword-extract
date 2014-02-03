e45c88ebc152e5efd91cda0fa54564ac33f0d2af
fasquelito <alexis.fasquel@insa-lyon.fr>
2014-02-03 11:53:35 +0100

    Adding punctuation

--- test/index.js
+++ test/index.js
@@ -4,8 +4,8 @@ require('should');
 var extractor = require('../lib/keyword-extract.js');
 
 describe('Simple extract', function() {
-	var txt1 = 'test et un autre testt';
-  it('should extract testt', function(done) {
+    var txt1 = 'test et un autre d\'testt';
+  it('should extract test', function(done) {
     extractor.extract('fr', txt1, function(words) {
     	words.should.eql(['testt']);
     	done();
@@ -13,7 +13,7 @@ describe('Simple extract', function() {
   });
 
 	var txt2 = 'et si on recharge sarce';
-  it('should extract testt', function(done) {
+  it('should extract sarce', function(done) {
     extractor.extract('fr', txt2, function(words) {
     	words.should.eql(['sarce']);
     	done();
