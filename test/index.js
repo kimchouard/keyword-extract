fee9512f66fb30616dd816376fb443641bd05d4e
fasquelito <alexis.fasquel@insa-lyon.fr>
2014-02-05 15:11:37 +0100

    Handling standelone figures

--- test/index.js
+++ test/index.js
@@ -4,16 +4,16 @@ require('should');
 var extractor = require('../lib/keyword-extract.js');
 
 describe('Simple extract', function() {
-    var txt1 = 'test et un autre d\'testt';
+    var txt1 = 'test et un autre d\'test2';
   it('should extract test', function(done) {
     extractor.extract('fr', txt1, function(words) {
-    	words.should.eql(['testt']);
+    	words.should.eql(['test2']);
     	done();
     });
   });
 
 	var txt2 = 'et si on recharge sarce';
-  it('should extract sarce', function(done) {
+  it('should extract sarce!', function(done) {
     extractor.extract('fr', txt2, function(words) {
     	words.should.eql(['sarce']);
     	done();
